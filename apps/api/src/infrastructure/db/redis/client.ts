9d1a2e37d9f03df2f5b26df5f178e32bdef5649b
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-11-14 01:23:27 -0300

    feat: adição auth do redis

--- apps/api/src/infrastructure/db/redis/client.ts
+++ apps/api/src/infrastructure/db/redis/client.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST || 'localhost',
     port: parseInt(process.env.REDIS_PORT || '6379'),
--- apps/api/src/infrastructure/queue/PlaylistSyncQueue.ts
+++ apps/api/src/infrastructure/queue/PlaylistSyncQueue.ts
@@ -8,6 +8,7 @@ export class PlaylistSyncQueue {
     const redisConfig = {
       host: process.env.REDIS_HOST || 'localhost',
       port: parseInt(process.env.REDIS_PORT || '6379'),
+      username: process.env.REDIS_USERNAME,
       password: process.env.REDIS_PASSWORD,
       db: parseInt(process.env.REDIS_DB || '0'),
       maxRetriesPerRequest: null,
