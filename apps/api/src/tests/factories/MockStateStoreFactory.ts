4ea2ba6c365ff9695dcbaec97120b87a9b3f89ef
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-29 16:27:32 -0300

    fix: corrige erro que poderia dar state leakage

--- apps/api/src/tests/factories/MockStateStoreFactory.ts
+++ apps/api/src/tests/factories/MockStateStoreFactory.ts
@@ -1,8 +1,8 @@
 import { IStateStore } from "@/application/ports/oauth/IStateStore";
 import { OAuthState } from "@/types/oauth/state";
 
-export const createMockOAuthStateStore: jest.Mocked<IStateStore<OAuthState>> = {
+export const createMockOAuthStateStore = (): jest.Mocked<IStateStore<OAuthState>> => ({
   get: jest.fn(),
   set: jest.fn(),
   delete: jest.fn()
-}
+})
