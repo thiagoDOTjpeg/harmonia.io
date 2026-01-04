bd2453cbba341db5ae50435213dbda0129a2a800
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-01-03 22:08:05 -0300

    refactor: muda de anemic domain para rich domain

--- apps/api/src/domain/entities/User.ts
+++ apps/api/src/domain/entities/User.ts
@@ -1,9 +1,57 @@
+import { IHasher } from "@/application/ports/crypto/IHasher";
+import { AppError } from "@harmonia/shared";
+import { Email } from "../value-objects/Email";
+
 export class User {
-  constructor(
-    public readonly id: string,
-    public email: string,
-    public name: string,
-    public passwordHash: string | null = null,
-    public emailVerifiedAt: Date | null,
+  private constructor(
+    private readonly _id: string,
+    private _email: Email,
+    private _name: string,
+    private _passwordHash: string | null = null,
+    private _emailVerifiedAt: Date | null,
   ) { }
+
+  static reconstitute(props: {
+    id: string;
+    email: string;
+    name: string;
+    passwordHash: string | null;
+    emailVerifiedAt: Date | null;
+  }): User {
+    return new User(
+      props.id,
+      Email.create(props.email),
+      props.name,
+      props.passwordHash,
+      props.emailVerifiedAt
+    );
+  }
+
+  get id(): string { return this._id; }
+  get email(): string { return this._email.getValue(); }
+  get name(): string { return this._name; }
+  get isVerified(): boolean { return this._emailVerifiedAt !== null; }
+
+  verifyEmail(): void {
+    if (this._emailVerifiedAt) {
+      throw new AppError("Email já verificado");
+    }
+    this._emailVerifiedAt = new Date();
+  }
+
+  async verifyPassword(plainPassword: string, hasher: IHasher): Promise<boolean> {
+    if (!this._passwordHash) return false;
+    return await hasher.verify(plainPassword, this._passwordHash);
+
+  }
+
+  changeEmail(newEmail: string): void {
+    this._email = Email.create(newEmail);
+    this._emailVerifiedAt = null;
+  }
+
+  hasPassword(): boolean {
+    return this._passwordHash !== null;
+  }
+
 }
